82ec6b1b2904591c6ce426c038c81f58ea71568d
André Nascimento <andre.neospace@gmail.com>
2026-06-03 11:08:49 -0300

    set up docker and save swarm token

--- master.sh
+++ master.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+# update and install docker
+sudo apt update -y
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://get.docker.com | bash
+sudo usermod -aG docker vagrant
+
+# start swarm and save worker join command
 sudo docker swarm init --advertise-addr=10.10.10.100
-sudo docker swarm join-token worker | grep docker > /vagrant/worker.sh
+sudo docker swarm join-token worker -q > /vagrant/swarm_token.txt
